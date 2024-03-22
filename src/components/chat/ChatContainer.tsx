d8a5228e3fa38a07ea66d44f340fcbeb6f86bc90
Pep3 Marquez <pep3.marquez@gmail.com>
2024-03-21 23:55:10 -0400

    prepare to export as library

--- src/components/chat/ChatContainer.tsx
+++ src/components/chat/ChatContainer.tsx
@@ -1,41 +1,22 @@
 import Chat, { Message } from "./Chat"
 import useChat from "@/hooks/useChat"
-import { useEffect } from "react"
-// testing socket.io
-import io from "socket.io-client"
-
-const socket = io('http://localhost:3000')
-
-const initMsgExample: Message[] = [
-  {
-    date: "12:01",
-    message: "Me alegra saber q estas bien! Sigue usando esta librería para tus proyectos!",
-    status: "read"
-  },
-  {
-    date: "12:00",
-    message: "Todo bien, gracias por escribirme!",
-    type: "received"
-  },
-  {
-    date: "12:00",
-    message: "Hola, como estas!",
-  },
-]
 
 const ChatContainer = () => {
-  const { chatComponent, onMessageReceived } = useChat({
-    initialMessages: initMsgExample,
-    onMessageSend: (message) => {
-      socket.emit('send-message', message)
+  const { 
+    chatComponent, 
+    // onMessageReceived // callback to receive message from the server
+  } = useChat({
+    onMessageSend: (message: Message) => {
+      message
+      // console.log('message sent', message) // send message to the server
     },
   })
 
-  useEffect(() => {
-    socket.on('receive-message', (message: Message) => {
-      onMessageReceived(message)
-    })
-  }, [onMessageReceived])
+  // useEffect(() => {
+  //   socket.on('receive-message', (message: Message) => {
+  //     onMessageReceived(message)
+  //   })
+  // }, [onMessageReceived])
 
   return (
     <div className="w-full sm:w-96 bg-slate-400 h-full rounded-xl overflow-hidden">
---
+++ src/index.ts
@@ -0,0 +1,4 @@
+import Chat from "./components/chat/Chat";
+import useChat from "./hooks/useChat";
+
+export { Chat, useChat };
